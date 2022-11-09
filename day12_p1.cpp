a1a3f17e1e0b6cee6ae5dd1de856008e8327db7b
Jakub <25148319+jakubekes@users.noreply.github.com>
2022-11-09 23:00:31 +0100

    update_day12

--- day12_p1.cpp
+++ day12_p1.cpp
@@ -2,15 +2,15 @@
 #include<fstream>
 #include<string>
 #include<vector>
+#include<algorithm>
 
 int main() {
 
 	std::ifstream myfile;
-	myfile.open("day11_input.txt", std::ios_base::in);
+	myfile.open("day12_input.txt", std::ios_base::in);
 	std::string temp;
-	unsigned long int answer{};
-	std::vector <std::vector<int>> v;
-	std::vector <int> tempv;
+	std::string ch{};
+	std::vector<std::string> v_temp;
 	
 	if (myfile.good()) {
 		std::cout << "File opened\n";
@@ -19,56 +19,139 @@ int main() {
 			//std::cout<<temp<<"\n";
 
 			for(int i=0; i<temp.length();i++){
-				tempv.push_back(temp[i]-48);				
-			}								
-			v.push_back(tempv);
-			tempv.clear();
+				if(temp[i]!='-'){
+					ch+=temp[i];
+				}else{
+					v_temp.push_back(ch);
+					ch="";
+					continue;
+				}			
+			}				
+			v_temp.push_back(ch);
+			ch="";
 		}
 		
-		for (int i=0; i<v.size(); i++){
-			for(int j=0; j<v[0].size(); j++){
-				std::cout<<v[i][j];
-			}
-				std::cout<<"\n";
+		std::sort(v_temp.begin(), v_temp.end());	
+				
+		for(int i=0; i<v_temp.size();i++){
+			std::cout<<v_temp[i]<<"\n";
 		}
 			
-		int stepper = 100;	
+		for(int i=0; i<v_temp.size()-1;i++){
+			if(v_temp[i]==v_temp[i+1]){
+				v_temp.erase (v_temp.begin() + i);
+				i--;
+			}
 			
-		while(stepper>0){
+		}
 		
-			for(int i=0; i<v.size(); i++){
-			for(int j=0; j<v[0].size(); j++){
-				v[i][j]++;
-				if(v[i][j]==0)v[i][j]++;
-			}
-			}
-			int stepflashes{1};
-			while(stepflashes>0){
-				stepflashes=0;
-				for(int i=0; i<v.size(); i++){
-					for(int j=0; j<v[0].size(); j++){
-						if (v[i][j]==-1)continue;
-						if (v[i][j]>9){
-							v[i][j]=-1;
-							stepflashes++;
-							for(int k=i-1; k<i+2; k++){
-							for(int l=j-1; l<j+2; l++){
-								if ( k<0 || l<0 || k>v.size()-1 || l>v[0].size()-1 || (k==i && l==j) || v[k][l]==-1)continue;
-								v[k][l]++;
-							}
-							}
-						}
-				
-					}				
-				}
-				answer+=stepflashes;
-				
-			}
+		std::cout<<"After dup remove:\n";
+		
+		std::vector<std::vector<std::string>> vec_2d;
+		std::vector<std::string> v_temp2;
+		for(int i=0; i<v_temp.size();i++){
+			std::cout<<v_temp[i]<<"\n";
+			if(v_temp[i]=="end")continue;
+			v_temp2.push_back(v_temp[i]);
+			vec_2d.push_back(v_temp2);
+			v_temp2.clear();			
+		}
+			
 
-			std::cout<<"Answer:"<< answer<<" stepper: "<<stepper<<"\n";
-			stepper--;
-		}			
-		std::cout<<"Answer:"<< answer;
+		
+
+		std::vector<std::string> vec_2d_temp, vec_2d_erase;
+		
+		for (int i=0; i<vec_2d.size(); i++){
+		myfile.clear();
+		myfile.seekg(0);
+			while (getline(myfile, temp)) {
+				//std::cout<<temp<<"\n";
+
+				for(int i=0; i<temp.length();i++){
+					if(temp[i]!='-'){
+						ch+=temp[i];
+					}else{
+						vec_2d_temp.push_back(ch);
+						ch="";
+						continue;
+					}			
+				}				
+				vec_2d_temp.push_back(ch);
+				ch="";
+				
+				
+				
+				if(vec_2d[i][0]==vec_2d_temp[0]){
+					if(vec_2d_temp[1]=="start"){
+						vec_2d_temp.clear();
+						continue;
+					}
+					vec_2d[i].push_back(vec_2d_temp[1]);
+					//vec_2d[i].push_back(vec_2d_temp[1]);
+				} else if(vec_2d[i][0]==vec_2d_temp[1]){
+					if(vec_2d_temp[0]=="start"){
+						vec_2d_temp.clear();
+						continue;	
+					}
+					vec_2d[i].push_back(vec_2d_temp[0]);
+					//vec_2d[i].push_back(vec_2d_temp[0]);
+				}
+				
+				vec_2d_temp.clear();
+			}
+			
+			//clearing:
+			//2. if cave has only 1x connection and it's small cave (that connection), then it's useless (delete it).
+			if(vec_2d[i].size()==2 && (vec_2d[i][1])[0]>=97 && (vec_2d[i][1])[0]<=122){
+				vec_2d_erase.push_back(vec_2d[i][0]);
+				vec_2d.erase (vec_2d.begin() + i);
+				i--;
+
+			}
+		}
+	
+		for(int i=0; i<vec_2d_erase.size();i++){
+			std::cout<<vec_2d_erase[i]<<" ";
+		}
+		std::cout<<"\n\n";
+		//clearing deleted above
+		for(int i=0; i<vec_2d.size();i++){
+			for(int j=0; j<vec_2d[i].size();j++){
+				for(int k=0; k<vec_2d_erase.size();k++){
+					if(vec_2d[i][j]==vec_2d_erase[k]){
+						vec_2d[i].erase (vec_2d[i].begin() + j);
+						j--;
+						break;
+					}
+				}
+				
+			}			
+		}
+		
+		std::cout<<"\nVec 2D:\n";
+		for(int i=0; i<vec_2d.size();i++){
+			for(int j=0; j<vec_2d[i].size();j++){
+				std::cout<<vec_2d[i][j]<<" ";
+			}
+			std::cout<<"\n";
+		}
+		
+		std::vector<std::vector<std::string>> vec_2d_operation = vec_2d;
+		
+		std::cout<<"\nVec 2D operation:\n";
+		for(int i=0; i<vec_2d_operation.size();i++){
+			for(int j=0; j<vec_2d_operation[i].size();j++){
+				std::cout<<vec_2d_operation[i][j]<<" ";
+			}
+			std::cout<<"\n";
+		}
+		
+
+		
+		
+		
+		
 		myfile.close();
 	}
 	else {
